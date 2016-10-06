7a798ce12e9a2572c8850968239c7fe4c1973039
Vyacheslav Vorobyov <vjache@gmail.com>
2016-10-06 19:30:55 +0300

    Make it compatible with Android.

--- src/main/java/org/klips/engine/rete/Consumer.kt
+++ src/main/java/org/klips/engine/rete/Consumer.kt
@@ -3,5 +3,4 @@ package org.klips.engine.rete
 import org.klips.engine.Binding
 import org.klips.engine.Modification
 
-@FunctionalInterface
 interface Consumer { fun consume(source: Node, mdf: Modification<Binding>)}
