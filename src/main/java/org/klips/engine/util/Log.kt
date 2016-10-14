21d255a5b613ce343be1faddd840be5f1b101ff7
Vyacheslav Vorobyov <vjache@gmail.com>
2016-10-15 00:22:49 +0300

    Log naming.

--- src/main/java/org/klips/engine/util/Log.kt
+++ src/main/java/org/klips/engine/util/Log.kt
@@ -4,25 +4,28 @@ import org.klips.engine.rete.Node
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
-class Log(val workingMemory : Boolean = false,
+class Log(val name : String = Random().nextInt().toString(),
+          val workingMemory : Boolean = false,
           val agenda : Boolean = false,
           val rete : Boolean = false) {
 
     inline fun wmEvent(init:() -> String?) {
         if (workingMemory)
-            init()?.let(::println)
+            init()?.let { println2(it) }
     }
 
     inline fun agEvent(init:() -> String?) {
         if (agenda)
-            init()?.let(::println)
+            init()?.let { println2(it) }
     }
 
     inline fun reteEvent(init:() -> String?) {
         if (rete)
-            init()?.let(::println)
+            init()?.let { println2(it) }
     }
 
 
     val nodeActivity = object : MutableMap<Node, Pair<AtomicInteger, AtomicInteger>> by HashMap(){}
+
+    fun println2(str:String) = println("[$name] $str")
 }
